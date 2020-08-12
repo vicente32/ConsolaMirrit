7e8bda345ee4593bf68871e35ac90ee199f82087
aribonomi <abonomi@acqua.net.ar>
2020-08-12 10:45:04 -0300

    Agrego ":8083"

--- consola/lib/src/providers/estadoDr/status_provider.dart
+++ consola/lib/src/providers/estadoDr/status_provider.dart
@@ -5,21 +5,21 @@ import 'dart:convert';
 class StatusProvider {
   final prefs = new PreferenciasUsuario();
 
-   Future<String>getStatus() async {
+  Future<String> getStatus() async {
     String username = prefs.user;
     String password = prefs.password;
-    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$username:$password'));
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$username:$password'));
     print(basicAuth);
 
-    final response = await get('https://${prefs.ip}/api/dragon/status',
+    final response = await get('https://${prefs.ip}:8083/api/dragon/status',
         headers: <String, String>{'Authorization': basicAuth});
     final decodedData = json.decode(response.body);
     print(decodedData);
 
     if (response.statusCode == 200) {
       return "Conexión exitosa";
-    }
-     else {
+    } else {
       return "Conexión fallida";
     }
   }
@@ -34,5 +34,3 @@ class StatusProvider {
 
 //   final response =
 //       await http.post(url, body: recepcionRequestToJson(recepcionRequest));
-
-
